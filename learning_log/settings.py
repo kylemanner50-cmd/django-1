500b62673e8c5663d4950bcbf43ac7e62ffd1a41
Kyle Manner <kyle_manner1@baylor.edu>
2026-04-16 01:05:25 -0500

    final commit

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d9xo4-*$atv3h%ps_#4h_8bx8yc-0z*kr%ugxyicpb5q+w!a%j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
